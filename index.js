f6b5d157b82d08d5a382d355f944ec611fead0d3
Lukasz Lucko <lukaszl95@o2.pl>
2018-01-27 13:11:41 +0100

    zadanie-13.7

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var fs = require('fs');
 var colors = require('colors');
 
-fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+fs.readdir('./', 'utf-8', function(err, data) {
     console.log('Dane przed zapisem!'.blue);
     console.log(data);
-    fs.writeFile('./tekst2.txt',data, function(err) {
+    fs.writeFile('./tekst.txt',data, function(err) {
         if (err) throw err;
         console.log('Zapisano!'.blue);
-        fs.readFile('./tekst2.txt', 'utf-8', function(err, data) {
+        fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
             console.log('Dane po zapisie'.blue)
             console.log(data);
         });
